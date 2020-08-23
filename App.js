a028e118eba6d73f58c9441e915154c054219dd3
Callie Morken <cmorken7468@scrippscollege.edu>
2020-08-22 23:22:25 -0700

    Added Google Fonts

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
-
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import EmojiDict from './src/components/EmojiDict';
-
 export default class App extends Component {
 	render() {
-		return <EmojiDict />;
+		return (
+			<View style={styles.container}>
+				<Text style={styles.welcome}>Hello World!</Text>
+			</View>
+		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF'
+	},
+	welcome: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+		fontSize: 50,
+		fontFamily: "OpenSansCondensed-Light"
+	}
+});
