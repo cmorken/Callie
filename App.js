8342b8cc7bcd4c660bd595a7a1a76781d3f8a89f
Callie Morken <cmorken7468@scrippscollege.edu>
2020-08-24 20:54:36 -0700

    Created Login Page

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
+/* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, StatusBar } from 'react-native';
 import EmojiDict from './src/components/EmojiDict';
-export default class App extends Component {
+
+type Props = {};
+export default class App extends Component<Props> {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text style={styles.welcome}>Hello World!</Text>
+				<StatusBar
+					backgroundColor = "1e90ff"
+					barStyle="light-content"
+					/>
+				<Text style={styles.welcome}>Login To My App</Text>
+				<TextInput
+					style={styles.input}
+					placeholder="Username"
+				/>
+				<TextInput
+					style={styles.input}
+					placeholder="Password"
+					secureTextEntry
+				/>
+				<View style={styles.btnContainer}>
+					<TouchableOpacity
+					style={styles.userBtn}
+					onPress={() => alert("Login Successful")}
+					>
+						<Text style={styles.btnText}>Login</Text>
+					</TouchableOpacity>
+					<TouchableOpacity
+						style={styles.userBtn}
+						onPress={() => alert("Signup Successful")}
+					>
+						<Text style={styles.btnText}>Signup</Text>
+					</TouchableOpacity>
+				</View>
 			</View>
 		);
 	}
@@ -16,13 +46,34 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
-		backgroundColor: '#F5FCFF'
+		backgroundColor: '#1e90ff'
 	},
 	welcome: {
 		textAlign: 'center',
 		color: '#333333',
 		marginBottom: 5,
 		fontSize: 50,
+		color: "#fff",
 		fontFamily: "OpenSansCondensed-Light"
+	},
+	input: {
+		width: "90%",
+		backgroundColor: "#fff",
+		padding: 15,
+		marginBottom: 10
+	},
+	btnContainer: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		width: "90%"
+	},
+	userBtn: {
+		backgroundColor: "#FFD700",
+		padding: 15,
+		width: "45%"
+	},
+	btnText: {
+		fontSize: 18,
+		textAlign: "center"
 	}
 });
